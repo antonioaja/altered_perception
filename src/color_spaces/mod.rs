71eaf75cdf9475b679f908f3a9984e1aa6583069
Antonio <97242516+antonioaja@users.noreply.github.com>
2023-03-20 13:29:21 -0400

    Reorganized into folders and moved test

---
+++ src/color_spaces/mod.rs
@@ -0,0 +1,4 @@
+pub mod cmyk;
+pub mod hsl;
+pub mod hsv;
+pub mod luma;
--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,8 @@
 //!
 //! Currently only supports conversion between HSV & RGB; Luma & RGB; HSL & RGB
 
-pub mod cmyk;
-pub mod hsl;
-pub mod hsv;
-pub mod luma;
+pub mod color_spaces;
+mod tests;
 
 #[cfg(feature = "serde")]
 #[macro_use]
@@ -106,80 +104,4 @@ pub struct CMYK<T> {
     pub k: T,
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::{Luma, HSL, HSV};
-    use rayon::prelude::*;
-    use rgb::RGB;
 
-    #[test]
-    fn rgb8_to_hsv_f64() {
-        (0..=u8::MAX).into_par_iter().for_each(|r| {
-            (0..=u8::MAX).for_each(|g| {
-                (0..=u8::MAX).for_each(|b| {
-                    let original = RGB::new(r, g, b);
-                    let intermediate: HSV<f64> = HSV::from_rgb::<u8>(original);
-                    let final_out = HSV::<f64>::to_rgb(intermediate);
-
-                    assert_eq!(original, final_out);
-                });
-            });
-        });
-    }
-
-    #[test]
-    fn rgb8_to_hsv_f32() {
-        (0..=u8::MAX).into_par_iter().for_each(|r| {
-            (0..=u8::MAX).for_each(|g| {
-                (0..=u8::MAX).for_each(|b| {
-                    let original = RGB::new(r, g, b);
-                    let intermediate: HSV<f32> = HSV::from_rgb::<u8>(original);
-                    let final_out = HSV::<f32>::to_rgb(intermediate);
-
-                    assert_eq!(original, final_out);
-                });
-            });
-        });
-    }
-
-    #[test]
-    fn rgb8_to_hsl_f64() {
-        (0..=u8::MAX).into_par_iter().for_each(|r| {
-            (0..=u8::MAX).for_each(|g| {
-                (0..=u8::MAX).for_each(|b| {
-                    let original = RGB::new(r, g, b);
-                    let intermediate: HSL<f64> = HSL::from_rgb::<u8>(original);
-                    let final_out = HSL::<f64>::to_rgb(intermediate);
-
-                    assert_eq!(original, final_out);
-                });
-            });
-        });
-    }
-
-    #[test]
-    fn rgb8_to_hsl_f32() {
-        (0..=u8::MAX).into_par_iter().for_each(|r| {
-            (0..=u8::MAX).for_each(|g| {
-                (0..=u8::MAX).for_each(|b| {
-                    let original = RGB::new(r, g, b);
-                    let intermediate: HSL<f32> = HSL::from_rgb::<u8>(original);
-                    let final_out = HSL::<f32>::to_rgb(intermediate);
-
-                    assert_eq!(original, final_out);
-                });
-            });
-        });
-    }
-
-    #[test]
-    fn luma8_to_rgb8() {
-        (0..=u8::MAX).into_par_iter().for_each(|r| {
-            let original = Luma::new(r);
-            let intermediate: RGB<u8> = Luma::to_rgb::<u8>(original);
-            let final_out = Luma::from_rgb(intermediate);
-
-            assert_eq!(original, final_out);
-        });
-    }
-}
